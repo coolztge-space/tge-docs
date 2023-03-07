7da45668b35f4cecf0a671a535065f76c0616690
TiagoVXII <tiago.alex.ramos.lobo@gmail.com>
2023-03-07 13:11:15 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,6 +115,7 @@ const config = {
         /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
         ({
           hashed: true,
+          docsRouteBasePath: '/',
         }),
       ],
     ],
