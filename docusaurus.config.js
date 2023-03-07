e1271f74bf7035a0070ddd4098e54a2681149025
TiagoVXII <tiago.alex.ramos.lobo@gmail.com>
2023-03-07 11:55:25 +0000

    Removal of editurl in "Blog"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,12 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/coolztge-space/tge-docs/tree/main/docs',
+            'https://github.com/coolztge-space/tge-docs/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/coolztge-space/tge-docs/tree/main/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
